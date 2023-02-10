4f0e96fc693e34ed3f618d0e6742e16058912f0f
Sheyla Cantalupo <sheylacantalupo.sc@gmail.com>
2023-02-10 09:34:23 -0300

    funções anônimas e textos dinâmicos

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,47 +1,50 @@
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+
+// passar como parâmetro o id da tag áudio
+
+function tocaSom (idTagAudio){
+    document.querySelector(idTagAudio).play();
 
 }
 
-function tocaSomClap (){
-    document.querySelector('#som_tecla_clap').play();
+// function tocaSomClap (){
+//     document.querySelector('#som_tecla_clap').play();
 
-}
+// }
 
-function tocaSomTim (){
-    document.querySelector('#som_tecla_tim').play();
+// function tocaSomTim (){
+//     document.querySelector('#som_tecla_tim').play();
 
-}
+// }
 
-function tocaSomPuff (){
-    document.querySelector('#som_tecla_puff').play();
+// function tocaSomPuff (){
+//     document.querySelector('#som_tecla_puff').play();
 
-}
+// }
 
-function tocaSomSplash (){
-    document.querySelector('#som_tecla_splash').play();
+// function tocaSomSplash (){
+//     document.querySelector('#som_tecla_splash').play();
 
-}
+// }
 
-function tocaSomToim (){
-    document.querySelector('#som_tecla_toim').play();
+// function tocaSomToim (){
+//     document.querySelector('#som_tecla_toim').play();
 
-}
+// }
 
-function tocaSomPsh (){
-    document.querySelector('#som_tecla_psh').play();
+// function tocaSomPsh (){
+//     document.querySelector('#som_tecla_psh').play();
 
-}
+// }
 
-function tocaSomTic (){
-    document.querySelector('#som_tecla_tic').play();
+// function tocaSomTic (){
+//     document.querySelector('#som_tecla_tic').play();
 
-}
+// }
 
-function tocaSomTom (){
-    document.querySelector('#som_tecla_tom').play();
+// function tocaSomTom (){
+//     document.querySelector('#som_tecla_tom').play();
 
-}
+// }
 
 // Quando a tecla for acionda vai chamar sua respectiva função
 
@@ -71,7 +74,18 @@ let contador = 0;
 
 while (contador < listaDeTeclas.length ) {
 
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    // console.log(idAudio);
+
+    tecla.onclick = function (){
+
+        tocaSom(idAudio)
+
+    }
 
     contador ++;
 
