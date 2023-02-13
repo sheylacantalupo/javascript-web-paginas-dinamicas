ac358d6a681af5296623c2b4d5a241046830ef09
Sheyla Cantalupo <sheylacantalupo.sc@gmail.com>
2023-02-13 12:15:08 -0300

    eventos do teclado

--- alura_fone/main.js
+++ alura_fone/main.js
@@ -2,11 +2,25 @@ const listaDeTeclas = document.querySelectorAll('input[type=button]');
 const input = document.querySelector('input[type=tel]');
 
 
-for (i = 0; i < listaDeTeclas.length; i++) {
+for (let i = 0; i < listaDeTeclas.length; i++) {
 
   const tecla = listaDeTeclas[i];
 
   tecla.onclick = function () {
     input.value = input.value + tecla.value;
+    
+  }
+
+  tecla.onkeydown = function (evento) {
+    if(evento.code === "Enter" || evento.code === "Space") {
+    tecla.classList.add('ativa');
+    }
+  }
+
+
+
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove('ativa');
   }
 }
